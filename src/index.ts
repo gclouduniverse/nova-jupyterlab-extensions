125bee0c1787edfa809d8510d6e5f89b164dacdb
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2019-02-04 04:28:49 +0000

    Version of the pluging that adds button to the panel

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,79 @@
 import {
+  IDisposable, DisposableDelegate
+} from '@phosphor/disposable';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
-import '../style/index.css';
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+import {
+  PageConfig 
+} from '@jupyterlab/coreutils';
 
 
 /**
- * Initialization data for the nova extension.
+ * The plugin registration information.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'nova',
-  autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension nova is activated!');
-  }
+const plugin: JupyterLabPlugin<void> = {
+  activate,
+  id: 'my-extension-name:buttonPlugin',
+  autoStart: true
 };
 
-export default extension;
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      	  let notebook_path = panel.context.contentsModel.path
+	  let a = context.path
+	  let b = context.localPath
+	  console.log(notebook_path)
+	  console.log(a)
+	  console.log(b) 
+	  console.log(PageConfig.getOption('serverRoot')) 
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClassName: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Ololo'
+    });
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterLab) {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+};
+
+
+/**
+ * Export the plugin as default.
+ */
+export default plugin;
